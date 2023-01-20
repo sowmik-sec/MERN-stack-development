f5f6a21c2d83b9d30b3c4437c4e72c3e23e5941b
sowmik-sec <sowmik.sec@gmail.com>
2023-01-20 11:59:12 +0600

    intro to node

---
+++ intro-to-node/index.js
@@ -0,0 +1,7 @@
+const superHeroes = require("superheroes");
+const superVillain = require("supervillains");
+const mySuperHeroName = superHeroes.random();
+console.log(mySuperHeroName);
+
+const mySuperVillain = superVillain.random();
+console.log(mySuperVillain);
