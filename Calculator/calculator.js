8f82433c9b1714b67b9dbd1acdf35000d829f00e
sowmik-sec <sowmik.sec@gmail.com>
2023-01-20 17:22:09 +0600

    simple calculator completed

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.send("Hello World of javascript!");
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body.num1);
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+  res.send("The answer of the calculation is " + (num1 + num2));
 });
 
 app.listen(3000, function () {
-  console.log("testing app.");
+  console.log("server is running on port 3000");
 });
