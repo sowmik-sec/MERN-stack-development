aad37b1d9290e2139605a4de19700df3b2484dc6
sowmik-sec <sowmik.sec@gmail.com>
2023-04-16 12:48:17 +0600

    using body parser to parse post request to the server

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=kurigram&units=metric&appid=dfbbc2973193a7a6990ff0319372be9a#";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.cityName);
+  const query = req.body.cityName;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=dfbbc2973193a7a6990ff0319372be9a#`;
   https.get(url, (response) => {
     console.log(response.statusCode);
     response.on("data", (data) => {
@@ -14,7 +22,7 @@ app.get("/", (req, res) => {
       const weatherDescription = weatherData.weather[0].description;
       const icon = ` https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
       res.write(
-        "<h1>The temperature in Kurigram is " + temp + " degrees Celsius.</h1>"
+        `<h1>The temperature in ${query} is ${temp} degrees Celsius.</h1>`
       );
       res.write("<h3>The weather is currently " + weatherDescription + "</h3>");
       res.write(`<img src=${icon}>`);
