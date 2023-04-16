4b39428c60fe9e5132fd948ad64ac093afdd58c6
sowmik-sec <sowmik.sec@gmail.com>
2023-04-16 12:03:18 +0600

    JSON pursed from response

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -7,7 +7,15 @@ app.get("/", (req, res) => {
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=kurigram&units=metric&appid=dfbbc2973193a7a6990ff0319372be9a#";
   https.get(url, (response) => {
-    console.log(response);
+    console.log(response.statusCode);
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      //   console.log(weatherData);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      console.log(weatherDescription);
+      console.log(temp);
+    });
   });
   res.send("Weather project is running");
 });
