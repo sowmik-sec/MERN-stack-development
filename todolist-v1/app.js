97481116906134e04503fa2601daf8503a5b586b
sowmik-sec <sowmik.sec@gmail.com>
2023-04-17 12:01:16 +0600

    send input value to server and read it from server

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   const today = new Date();
@@ -14,6 +15,11 @@ app.get("/", (req, res) => {
   };
   const day = today.toLocaleDateString("en-in", options);
   res.render("list", { dayName: day });
+});
+
+app.post("/", (req, res) => {
+  const item = req.body.newItem;
+  console.log(item);
 });
 
 app.listen(port, () => {
