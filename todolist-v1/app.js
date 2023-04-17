ebaca91d1b42ba07cd3a8797d7ba9971f2f4284f
sowmik-sec <sowmik.sec@gmail.com>
2023-04-17 14:02:42 +0600

    add pre-made css stylesheets to website

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 5000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
 
